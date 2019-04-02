3a8075f20c427600bfee9d6329f3b1f8e0f344bd
Ricardo Moura <ricardo.moura@mindera.com>
2019-04-02 14:06:05 +0100

    No need of random prototype

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 
 module.exports = (() => {
-  faker.random.prototype.alpha = (count = 1) => {
+  faker.random.alpha = (count = 1) => {
     let wholeString = "";
     for(let i = 0; i < count; i++) {
       wholeString += faker.random.arrayElement(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
